b727b5e6f6e75820f376f7654183fafb09ef6dc9
Oleg <kalinon7@gmail.com>
2016-03-08 12:02:39 +0300

    boilerplate for webpack and react

---
+++ app/Greeter.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import config  from './config.json';
+
+import styles from './greeter.css';
+
+export default class Greeter extends Component{
+    render(){
+        return (
+            <div className={styles.root}>
+                {config.greetText}, Олег
+            </div>
+        );
+    }
+}
