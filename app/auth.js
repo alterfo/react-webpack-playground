1cfd407b987574c1105e93c6e923e45d9fca455f
osidorkin <osidorkin@i-novus.ru>
2016-03-09 21:42:53 +0300

    [nf] Simplest auth ever

--- app/auth.js
+++ app/auth.js
@@ -1,41 +1,49 @@
 module.exports = {
     login(email, pass, cb) {
+        cb = arguments[arguments.length - 1]
         if (localStorage.token) {
-            cb && cb(true);
+            if (cb) cb(true)
+            this.onChange(true)
+            return
         }
         pretendRequest(email, pass, (res) => {
             if (res.authenticated) {
-                localStorage.token = res.token;
-                cb && cb(true)
+                localStorage.token = res.token
+                if (cb) cb(true)
+                this.onChange(true)
             } else {
-                cb && cb(false)
+                if (cb) cb(false)
+                this.onChange(false)
             }
         })
     },
 
     getToken() {
-        return localStorage.token;
+        return localStorage.token
     },
 
     logout(cb) {
-        delete localStorage.token;
-        cb && cb()
+        delete localStorage.token
+        if (cb) cb()
+        this.onChange(false)
     },
-    loggedIn() {
-        return !!localStorage.token;
-    }
 
-};
+    loggedIn() {
+        return !!localStorage.token
+    },
+
+    onChange() {}
+}
 
 function pretendRequest(email, pass, cb) {
     setTimeout(() => {
-        if (email === 'test@test.test' && pass === 'testtest1') {
+        if (email === 'joe@example.com' && pass === 'password1') {
             cb({
                 authenticated: true,
                 token: Math.random().toString(36).substring(7)
             })
         } else {
-            cb({authenticated: false})
+            cb({ authenticated: false })
         }
     }, 0)
 }
